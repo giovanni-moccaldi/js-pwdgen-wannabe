6005092294124aa4f68a58750efeb77dc8c5a633
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-20 17:00:51 +0100

    prompt

--- file-js/myscrypt-js.js
+++ file-js/myscrypt-js.js
@@ -1,6 +1,15 @@
-console.log("ciao");
-let nome = "giovanni";
-console.log(nome);
+// console.log("ciao");
+// let nome = "giovanni";
+// console.log(nome);
 
 
-document.getElementById(ciao)
+// document.getElementById(ciao)
+
+
+
+
+let nome = prompt("come ti chiami");
+let cognome = prompt("qual'e' il tuo cognome");
+let colore = prompt("colore preferito?");
+let anni = prompt("quanti anni?");
+document.getElementById('info').innerHTML;
